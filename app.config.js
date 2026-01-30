cb15c03887b6eae5401662d953f13173c2339ccc
flaczq <flaczq@gmail.com>
2026-01-30 10:04:04 +0100

    ADMOB_APP_ID

---
+++ app.config.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+
+export default ({ config }) => {
+  return {
+    ...config,
+    name: "Pure Highlights 2.0",
+    slug: "pure-highlights-2",
+    plugins: [
+      [
+        "react-native-google-mobile-ads",
+        {
+          androidAppId: process.env.ADMOB_APP_ID
+        }
+      ]
+    ]
+  };
+};
